59794ff4998cb3d55a49b95e9b805ddf932d39b2
batista7557 <gabriel.bat2001@gmail.com>
2023-02-13 21:50:24 -0300

    Menu principal

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ window.geometry("1280x720")
 text_area = tkinter.Text(window, font="Arial 20 bold")
 text_area.pack()
 
+main_menu = tkinter.Menu(window)
+main_menu.add_cascade(label="File")
+window.config(menu=main_menu)
 window.mainloop()
