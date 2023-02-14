20edcb1b01af2511ad30c76ccafea33fd35908ea
batista7557 <gabriel.bat2001@gmail.com>
2023-02-13 21:44:57 -0300

    Área de texto

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import tkinter
 window = tkinter.Tk()
 window.title("Notepad")
 window.geometry("1280x720")
+
+text_area = tkinter.Text(window, font="Arial 20 bold")
+text_area.pack()
+
 window.mainloop()
