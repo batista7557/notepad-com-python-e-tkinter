06d89c51676b2cf1949001197372bdf1d5629733
batista7557 <gabriel.bat2001@gmail.com>
2023-02-14 22:55:45 -0300

    Opções do menu

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ text_area = tkinter.Text(window, font="Arial 20 bold")
 text_area.pack()
 
 main_menu = tkinter.Menu(window)
-main_menu.add_cascade(label="File")
+
+file_menu = tkinter.Menu(main_menu)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Save")
+file_menu.add_command(label="Save as...")
+
+main_menu.add_cascade(label="File", menu=file_menu)
 window.config(menu=main_menu)
 window.mainloop()
