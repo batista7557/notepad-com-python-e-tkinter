9c329a1c3361d2a8ce7f965c34e1a259224a923f
batista7557 <gabriel.bat2001@gmail.com>
2023-02-15 23:31:57 -0300

    Comandos para o menu, função de sair e ajuste do text_area

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ window = tkinter.Tk()
 window.title("Notepad")
 window.geometry("1280x720")
 
-text_area = tkinter.Text(window, font="Arial 20 bold")
+text_area = tkinter.Text(window, font="Arial 20 bold", width=1280, height=720)
 text_area.pack()
 
 main_menu = tkinter.Menu(window)
 
-file_menu = tkinter.Menu(main_menu)
-file_menu.add_command(label="New")
-file_menu.add_command(label="Save")
-file_menu.add_command(label="Save as...")
+file_menu = tkinter.Menu(main_menu, tearoff=0)
+file_menu.add_command(label="New", command=None)
+file_menu.add_command(label="Save", command=None)
+file_menu.add_command(label="Save as...", command=None)
+file_menu.add_command(label="Exit", command=window.quit)
 
 main_menu.add_cascade(label="File", menu=file_menu)
 window.config(menu=main_menu)
