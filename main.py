bf07e48e24204d89f5d397421269b96e510b7187
batista7557 <gabriel.bat2001@gmail.com>
2023-02-16 22:48:21 -0300

    Criando funções do menu

--- main.py
+++ main.py
@@ -9,12 +9,22 @@ text_area.pack()
 
 main_menu = tkinter.Menu(window)
 
+def NewFile():
+    print("Crie um novo arquivo")
+
+def Save():
+    print("Arquivo salvo")
+
+def Save_as():
+    print("Salvar como...")
+
 file_menu = tkinter.Menu(main_menu, tearoff=0)
-file_menu.add_command(label="New", command=None)
-file_menu.add_command(label="Save", command=None)
-file_menu.add_command(label="Save as...", command=None)
+file_menu.add_command(label="New", command=NewFile)
+file_menu.add_command(label="Save", command=Save)
+file_menu.add_command(label="Save as...", command=Save_as)
 file_menu.add_command(label="Exit", command=window.quit)
 
 main_menu.add_cascade(label="File", menu=file_menu)
 window.config(menu=main_menu)
 window.mainloop()
+
